3c04128d81d58a69c09912d126ff6cf4845fb58c
ali asaria <aliasaria@users.noreply.github.com>
2024-01-02 10:36:26 -0500

    fix embeddings page

--- src/renderer/components/Experiment/Embeddings.tsx
+++ src/renderer/components/Experiment/Embeddings.tsx
@@ -8,6 +8,7 @@ import Sheet from '@mui/joy/Sheet';
 import * as chatAPI from 'renderer/lib/transformerlab-api-sdk';
 import { PencilIcon, PlayIcon } from 'lucide-react';
 import {
+  Box,
   Button,
   FormControl,
   FormHelperText,
@@ -34,48 +35,60 @@ export default function Embeddings({ model_name }) {
 
     embeddings = embeddings.join('\n\n\n');
 
-    document.getElementsByName('outputText')[0].value = embeddings;
+    document.getElementById('embeddingsResult').innerHTML = embeddings;
   }
 
   return (
     <>
-      <Sheet>
+      <Sheet
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          marginBottom: '1rem',
+          paddingBottom: '1rem',
+          overflowY: 'auto',
+          overflowX: 'hidden',
+          padding: '1rem',
+        }}
+      >
         <Typography level="h1" mb={3}>
           Generate Embeddings
         </Typography>
-
-        <FormControl>
-          <FormLabel>Input Text</FormLabel>
-          <Textarea
-            minRows={8}
-            size="lg"
-            defaultValue="This is a line
+        <div>
+          <FormControl>
+            <FormLabel>Input Text</FormLabel>
+            <Textarea
+              minRows={8}
+              size="lg"
+              defaultValue="This is a line
 This is a second line."
-            name="inputText"
-          />
+              name="inputText"
+            />
 
-          <FormHelperText>
-            Enter text to convert, one input per line.
-          </FormHelperText>
-        </FormControl>
-        <Button
-          sx={{ mt: 4, mb: 4 }}
-          startDecorator={<PlayIcon />}
-          onClick={async () => await getEmbeddings()}
-        >
-          Process Embeddings
-        </Button>
-
-        <FormControl>
-          <FormLabel>Output Vectors</FormLabel>
-          <Textarea
-            minRows={8}
-            maxRows={20}
-            size="lg"
-            name="outputText"
-            sx={{ whiteSpace: 'nowrap' }}
-          />
-        </FormControl>
+            <FormHelperText>
+              Enter text to convert, one input per line.
+            </FormHelperText>
+          </FormControl>
+          <Button
+            sx={{ mt: 4, mb: 4 }}
+            startDecorator={<PlayIcon />}
+            onClick={async () => await getEmbeddings()}
+          >
+            Process Embeddings
+          </Button>
+        </div>
+        <div>
+          <FormControl>
+            <FormLabel>Output Vectors</FormLabel>
+            <Sheet
+              id="embeddingsResult"
+              variant="soft"
+              color="neutral"
+              sx={{ padding: 1, overflow: 'hidden' }}
+            />
+          </FormControl>
+        </div>
       </Sheet>
     </>
   );
