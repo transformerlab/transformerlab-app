748a8dc5dec00cdcfc368fceec8ed989c5d678fa
ali asaria <aliasaria@users.noreply.github.com>
2024-01-24 10:21:21 -0500

    add mlx logo in store

---
+++ src/renderer/components/Shared/TinyMLXLogo.tsx
@@ -0,0 +1,32 @@
+export default function TinyMLXLogo() {
+  return (
+    <svg
+      // class="text-black inline-block ml-2 text-sm"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      aria-hidden="true"
+      focusable="false"
+      role="img"
+      preserveAspectRatio="xMidYMid meet"
+      width="2em"
+      height="1em"
+      viewBox="0 0 25 13"
+    >
+      <path
+        fill="#000"
+        fill-rule="evenodd"
+        d="M24.17.96H1.5v1.08H.42v10.77-1.08h1.09v1.08h21.57v-1.08h1.09V2.04h-1.09V.96h1.09Z"
+        clip-rule="evenodd"
+      ></path>
+      <path fill="#fff" d="M10.6 3.6h1.12v5.26h1.55v1.31h-2.68V3.6Z"></path>
+      <path
+        fill="#D5D5D5"
+        d="M14.16 3.6v1.32h2.63v3.94h-2.63v1.31h1.31V7.55h3.94v2.62h1.32V8.86H18.1V4.92h2.63V3.6H19.4v2.63h-3.94V3.6h-1.31Z"
+      ></path>
+      <path
+        fill="#fff"
+        d="M5.2 3.6H4.06v6.57h1.12V6.23h3.39v3.94H9.7V3.6H8.58v1.32H7.45v2.63H6.32V4.92H5.2V3.6Z"
+      ></path>
+    </svg>
+  );
+}
