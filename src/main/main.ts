a587c3441c4bd198aed7eb6c6214d0cc824c45a7
ali asaria <ali.asaria@gmail.com>
2024-03-01 12:36:08 -0500

    does this one line change turn on autoupdate?

--- src/main/main.ts
+++ src/main/main.ts
@@ -166,7 +166,7 @@ const createWindow = async () => {
 
   // Remove this if your app does not use auto updates
   // eslint-disable-next-line
-  //new AppUpdater();
+  new AppUpdater();
 };
 
 const appAboutOptions = {
