15e0110a90355cdef104434fe946c55f59e3ea42
Tony Salomone <dadmobile@gmail.com>
2024-03-22 16:04:53 -0400

    download_windows_api.bat now uses install.sh through WSL

--- download_windows_api.bat
+++ download_windows_api.bat
@@ -1,10 +1,18 @@
 @echo off
 
-echo Downloading windows API installer
-set INSTALLER_URL=https://raw.githubusercontent.com/transformerlab/transformerlab-api/main/install_windows.bat
-call curl %INSTALLER_URL% -o install_windows.bat
-call install_windows.bat download_transformer_lab
-echo TransformerLab API download complete. Errorlevel=%ERRORLEVEL%
+@rem Determine if WSL is installed and set WSL_HOMEDIR
+set WSL_CMD=wsl wslpath -w ~
+for /F %%G In ('%WSL_CMD%') do set "WSL_HOMEDIR=%%G"
+if %ERRORLEVEL%==1 (
+  echo Error accessing WSL home directory.
+  echo TransformerLab requires Windows 10 or later with WSL installed.
+  EXIT /B 1
+)
 
-@rem Clean up
-del install_windows.bat
+echo Downloading windows API installer using WSL.
+set INSTALLER_URL=https://raw.githubusercontent.com/transformerlab/transformerlab-api/main/install.sh
+set INSTALLER_FILENAME=install.sh
+call wsl curl %INSTALLER_URL% -o %INSTALLER_FILENAME%
+call wsl ./%INSTALLER_FILENAME% download_transformer_lab
+echo TransformerLab API download complete. Errorlevel=%ERRORLEVEL%
+call wsl rm ./%INSTALLER_FILENAME%
