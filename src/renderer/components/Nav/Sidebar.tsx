1a802d819d247ca0763511cd53486b7f6e85c49c
Ali Asaria <ali.asaria@gmail.com>
2024-04-12 09:10:15 -0400

    thin icons a bit

--- src/renderer/components/Nav/Sidebar.tsx
+++ src/renderer/components/Nav/Sidebar.tsx
@@ -87,6 +87,13 @@ export default function Sidebar({
         userSelect: 'none',
         width: '100%',
         backgroundColor: 'rgb(214,207,225,0.5)',
+        '& .lucide': {
+          strokeWidth: '1.5px',
+          color: 'var(--joy-palette-neutral-700)',
+        },
+        '& .MuiBadge-root': {
+          color: 'var(--joy-palette-neutral-700)',
+        },
       }}
     >
       <SelectExperimentMenu
