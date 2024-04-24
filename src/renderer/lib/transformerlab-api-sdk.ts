aaf48732f3cc050e575f80061b1941f7835ea00f
Tony Salomone <dadmobile@gmail.com>
2024-04-24 16:53:40 -0400

    Add uninstalled param to GetHFCache SDK call

--- src/renderer/lib/transformerlab-api-sdk.ts
+++ src/renderer/lib/transformerlab-api-sdk.ts
@@ -499,7 +499,8 @@ Endpoints.Models = {
     API_URL() + 'model/details/' + convertSlashInUrl(modelId),
   GetLocalHFConfig: (modelId: string) =>
     API_URL() + 'model/get_local_hfconfig?model_id=' + modelId,
-  GetHFCacheModelList: () => API_URL() + 'model/hfcache_list',
+  GetHFCacheModelList: (uninstalled_only: boolean = true) =>
+    API_URL() + 'model/hfcache_list?uninstalled_only=' + uninstalled_only,
   ImportHFCacheModels: () => API_URL() + 'model/hfcache_import',
   HuggingFaceLogin: () => API_URL() + 'model/login_to_huggingface',
   Delete: (modelId: string) => API_URL() + 'model/delete?model_id=' + modelId,
