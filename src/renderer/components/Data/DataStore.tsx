c62ba1bd56a386f5539f838e0c8588900853b676
safiyamak <safiyam123579@gmail.com>
2024-05-21 13:51:53 -0400

    Adding search to Data Store

--- src/renderer/components/Data/DataStore.tsx
+++ src/renderer/components/Data/DataStore.tsx
@@ -1,13 +1,27 @@
 import useSWR from 'swr';
+import { useState } from 'react';
 
-import { Grid, LinearProgress, Sheet } from '@mui/joy';
+import {
+  FormControl,
+  FormLabel,
+  Grid,
+  Input,
+  LinearProgress,
+  Sheet,
+  Box,
+} from '@mui/joy';
 import DatasetCard from './DatasetCard';
+import { SearchIcon } from 'lucide-react';
+import { filterByFilters } from 'renderer/lib/utils';
 
 import * as chatAPI from '../../lib/transformerlab-api-sdk';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function DataStore() {
+  const [searchText, setSearchText] = useState('');
+  const [filters, setFilters] = useState({});
+
   const { data, error, isLoading, mutate } = useSWR(
     chatAPI.Endpoints.Dataset.Gallery(),
     fetcher
@@ -16,35 +30,64 @@ export default function DataStore() {
   if (error) return 'An error has occurred.';
   if (isLoading) return <LinearProgress />;
   return (
-    <Sheet
-      className="OrderTableContainer"
-      variant="outlined"
-      sx={{
-        width: '100%',
-        height: '100%',
-        borderRadius: 'md',
-        flex: 1,
-        overflow: 'auto',
-        minHeight: 0,
-        padding: 2,
-      }}
-    >
-      <Grid container spacing={2} sx={{ flexGrow: 1 }}>
-        {data.map((row) => (
-          <Grid xs={4}>
-            <DatasetCard
-              name={row.name}
-              size={row.size}
-              key={row.id}
-              description={row.description}
-              repo={row.huggingfacerepo}
-              download
-              location={undefined}
-              parentMutate={mutate}
-            />
-          </Grid>
-        ))}
-      </Grid>
-    </Sheet>
+    <>
+      <Box
+        className="SearchAndFilters-tabletUp"
+        sx={{
+          borderRadius: 'sm',
+          pb: 2,
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: 1.5,
+          '& > *': {
+            minWidth: {
+              xs: '120px',
+              md: '160px',
+            },
+          },
+        }}
+      >
+        <FormControl sx={{ flex: 2 }} size="sm">
+          <FormLabel>&nbsp;</FormLabel>
+          <Input
+            placeholder="Search"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            startDecorator={<SearchIcon />}
+          />
+        </FormControl>
+      </Box>
+      <Sheet
+        className="OrderTableContainer"
+        variant="outlined"
+        sx={{
+          width: '100%',
+          height: '100%',
+          borderRadius: 'md',
+          flex: 1,
+          overflow: 'auto',
+          minHeight: 0,
+          padding: 2,
+        }}
+      >
+        <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+          {data &&
+            filterByFilters(data, searchText).map((row) => (
+              <Grid xs={4}>
+                <DatasetCard
+                  name={row.name}
+                  size={row.size}
+                  key={row.id}
+                  description={row.description}
+                  repo={row.huggingfacerepo}
+                  download
+                  location={undefined}
+                  parentMutate={mutate}
+                />
+              </Grid>
+            ))}
+        </Grid>
+      </Sheet>
+    </>
   );
 }
