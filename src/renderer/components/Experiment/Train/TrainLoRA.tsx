f3458abd2c9b71d9b7df4879fc521ac71d21cd13
ali asaria <aliasaria@users.noreply.github.com>
2024-07-18 11:37:36 -0400

    fix text

--- src/renderer/components/Experiment/Train/TrainLoRA.tsx
+++ src/renderer/components/Experiment/Train/TrainLoRA.tsx
@@ -214,7 +214,7 @@ export default function TrainLoRA({ experimentInfo }) {
               {/* <th>Description</th> */}
               <th width="120px">Dataset</th>
               <th width="300px">Plugin</th>
-              <th width="400px">Data</th>
+              <th width="400px">Config</th>
               <th style={{ textAlign: 'right' }}>&nbsp;</th>
             </thead>
             <tbody>
