2da843380680bc95393a1885c7f55a067c701bf3
Tony Salomone <dadmobile@gmail.com>
2024-09-05 11:33:02 -0400

    Use the backend provided system prompt to dynamically load tool list

--- src/renderer/components/Experiment/Interact/Interact.tsx
+++ src/renderer/components/Experiment/Interact/Interact.tsx
@@ -36,23 +36,17 @@ function scrollChatToBottom() {
   setTimeout(() => document.getElementById('endofchat')?.scrollIntoView(), 400);
 }
 
-// TODO: Make this call the backend to get the list of tools that are available
-// For now this is a static message
+// Get the System Message from the backend.
+// Returns "" if there was an error.
 function getAgentSystemMessage() {
-  return `You are a function calling AI model. You are provided with function signatures within <tools></tools> XML tags. You may call one or more functions to assist with the user query. Don't make assumptions about what values to plug into functions. Here are the available tools: <tools> {"type": "function", "function": {"name": "get_current_temperature", "description": "get_current_temperature(location: str) - Gets the temperature at a given location.
-
-    Args:
-        location(str): The location to get the temperature for, in the format "city, country"", "parameters": {"type": "object", "properties": {"location": {"type": "string", "description": "The location to get the temperature for, in the format \"city, country\""}}, "required": ["location"]}}
-{"type": "function", "function": {"name": "get_current_wind_speed", "description": "get_current_wind_speed(location: str) -> float - Get the current wind speed in km/h at a given location.
-
-    Args:
-        location(str): The location to get the temperature for, in the format "City, Country"
-    Returns:
-        The current wind speed at the given location in km/h, as a float.", "parameters": {"type": "object", "properties": {"location": {"type": "string", "description": "The location to get the temperature for, in the format \"City, Country\""}}, "required": ["location"]}} </tools>Use the following pydantic model json schema for each tool call you will make: {"properties": {"name": {"title": "Name", "type": "string"}, "arguments": {"title": "Arguments", "type": "object"}}, "required": ["name", "arguments"], "title": "FunctionCall", "type": "object"}}
-For each function call return a json object with function name and arguments within <tool_call></tool_call> XML tags as follows:
-<tool_call>
-{"name": <function-name>, "arguments": <args-dict>}
-</tool_call>`
+  const prompt = await fetch(
+    chatAPI.Endpoints.Tools.Prompt()
+  ).then(
+    (res) => res.json()
+  ).catch(
+    (error) => ""
+  );
+  return prompt;
 }
 
 /**
