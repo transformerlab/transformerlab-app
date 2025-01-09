d6a76bc5e27245c1c7abd78475c60fa908e6fe21
Tony Salomone <dadmobile@gmail.com>
2025-01-09 12:00:25 -0500

    Add a shared component for showing a download progress bar.

---
+++ src/renderer/components/Shared/CurrentDownloadBox.tsx
@@ -0,0 +1,82 @@
+import {
+    Box,
+    Chip,
+    LinearProgress,
+    Sheet,
+    Stack,
+    Typography,
+} from '@mui/joy';
+
+import {
+    formatBytes,
+} from '../../lib/utils';
+
+export default function CurrentDownloadBox({ jobId, assetName }) {
+
+  const modelDownloadProgress = false;
+
+  return (
+    <>
+      {jobId && (
+        <Box>
+          {/* <Typography level="title-md" sx={{ mt: 2 }}>
+            Downloading
+          </Typography> */}
+          <Stack>
+            {/* Download Progress: {JSON.stringify(modelDownloadProgress)}
+            Currently Downloading: {JSON.stringify(currentlyDownloading)}&nbsp;
+            Job: {JSON.stringify(jobId)} */}
+            <Sheet
+              variant="soft"
+              color="warning"
+              sx={{ my: 1, padding: 2, borderRadius: '8px' }}
+            >
+              <Typography level="title-sm" sx={{ pb: 1 }}>
+                Downloading
+                <Chip variant="soft">{assetName}</Chip>
+                {' - '}
+                {modelDownloadProgress?.job_data?.total_size_of_model_in_mb >
+                  0 && (
+                  <>
+                    {clamp(
+                      Number.parseFloat(modelDownloadProgress?.progress),
+                      0,
+                      100
+                    ).toFixed(0)}
+                    % {' - '}
+                  </>
+                )}
+                <>
+                  {modelDownloadProgress?.job_data?.downloaded != 0
+                    ? formatBytes(
+                        modelDownloadProgress?.job_data?.downloaded *
+                          1024 *
+                          1024
+                      )
+                    : 'Download Starting'}
+                  ↓
+                </>
+              </Typography>
+              {modelDownloadProgress?.progress !== -1 && (
+                <>
+                  {modelDownloadProgress?.job_data?.total_size_of_model_in_mb >
+                  0 ? (
+                    <LinearProgress
+                      determinate
+                      value={clamp(modelDownloadProgress?.progress, 0, 100)}
+                    />
+                  ) : (
+                    <LinearProgress />
+                  )}
+                </>
+              )}
+            </Sheet>
+          </Stack>
+          {/* downloadprogress: {JSON.stringify(modelDownloadProgress)} - currdown:{' '}
+          {JSON.stringify(currentlyDownloading)} - jobid:{' '}
+          {JSON.stringify(jobId)} */}
+        </Box>
+      )}
+    </>
+  );
+}
