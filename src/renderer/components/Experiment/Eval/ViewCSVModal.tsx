4f9669b05b153b850c0ee6641f52dc1c170f82ab
ali asaria <aliasaria@users.noreply.github.com>
2025-02-06 14:41:51 -0500

    simplify opencsv modal

--- src/renderer/components/Experiment/Eval/ViewCSVModal.tsx
+++ src/renderer/components/Experiment/Eval/ViewCSVModal.tsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { Modal, Box, Typography, IconButton, ModalClose, ModalDialog } from '@mui/joy';
+import {
+  Modal,
+  Box,
+  Typography,
+  IconButton,
+  ModalClose,
+  ModalDialog,
+} from '@mui/joy';
 
 const ViewCSVModal = ({ open, onClose, jobId, fetchCSV }) => {
   const [csvText, setCsvText] = useState('');
 
   useEffect(() => {
     if (open && jobId) {
-      fetchCSV(jobId).then(data => {
+      fetchCSV(jobId).then((data) => {
         setCsvText(data);
       });
     }
@@ -14,23 +21,14 @@ const ViewCSVModal = ({ open, onClose, jobId, fetchCSV }) => {
 
   return (
     <Modal open={open} onClose={onClose}>
-      <ModalDialog sx={{ width: '80vw', height: '80vh' }}>
+      <ModalDialog sx={{ width: '90vw', height: '90vh' }}>
         <ModalClose />
-      <Box
-          sx={{
-            height: '100%',
-            overflow: 'hidden',
-            border: '10px solid #444',
-            padding: '0rem 0 0 1rem',
-            backgroundColor: '#fff',
-            width: '100%',
-          }}
-        >
         <Typography level="h4" mb={2}>
           Additional Output from Job: {jobId}
         </Typography>
-        <pre>{csvText}</pre>
-      </Box>
+        <Box>
+          <pre>{csvText}</pre>
+        </Box>
       </ModalDialog>
     </Modal>
   );
