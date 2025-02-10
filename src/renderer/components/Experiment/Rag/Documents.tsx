9818b02df71200f30d1c8b9455fac56efd39ce91
ali asaria <aliasaria@users.noreply.github.com>
2025-02-10 11:50:10 -0500

    Name root of documents folder

--- src/renderer/components/Experiment/Rag/Documents.tsx
+++ src/renderer/components/Experiment/Rag/Documents.tsx
@@ -456,7 +456,7 @@ export default function Documents({ experimentInfo, fullPage = false }) {
         <div>
           {loading && <CircularProgress size="sm" />}
           {currentFolder == '' ? (
-            '/'
+            'Documents'
           ) : (
             <>
               <Link
@@ -464,7 +464,7 @@ export default function Documents({ experimentInfo, fullPage = false }) {
                   setCurrentFolder('');
                 }}
               >
-                .. /
+                Documents /
               </Link>{' '}
               {currentFolder} /
             </>
