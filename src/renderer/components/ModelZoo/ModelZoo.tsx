7a3dd29210b5036ba2f90cd65ef566df92cda50d
abhimazu <mail.abhijeetmazumdar@gmail.com>
2025-03-03 12:37:50 -0500

    Reverting back the package json files

--- src/renderer/components/ModelZoo/ModelZoo.tsx
+++ src/renderer/components/ModelZoo/ModelZoo.tsx
@@ -64,7 +64,7 @@ export default function ModelZoo({ experimentInfo, tab = 'store' }) {
         >
           <ModelStore />
         </TabPanel>
-          </Tabs>
+      </Tabs>
     </Sheet>
   );
 }
