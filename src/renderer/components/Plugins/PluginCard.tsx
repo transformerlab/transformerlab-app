842006bb0aa92e6373db3062d07f652cdd09522a
ali asaria <aliasaria@users.noreply.github.com>
2025-03-10 12:57:04 -0400

    how supported architectures

--- src/renderer/components/Plugins/PluginCard.tsx
+++ src/renderer/components/Plugins/PluginCard.tsx
@@ -2,29 +2,15 @@ import Button from '@mui/joy/Button';
 import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
-import {
-  ArrowRightFromLineIcon,
-  Divide,
-  DownloadIcon,
-  FolderSearch2Icon,
-  GraduationCapIcon,
-  HelpCircleIcon,
-  RocketIcon,
-  RotateCcwIcon,
-} from 'lucide-react';
+import { DownloadIcon, RotateCcwIcon, Type } from 'lucide-react';
 
 import * as chatAPI from '../../lib/transformerlab-api-sdk';
-import {
-  AspectRatio,
-  Box,
-  ButtonGroup,
-  CardActions,
-  Chip,
-  CircularProgress,
-  Divider,
-} from '@mui/joy';
+import { Box, Chip, CircularProgress, Stack } from '@mui/joy';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+
+import TinyMLXLogo from '../Shared/TinyMLXLogo';
+import TinyNVIDIALogo from '../Shared/TinyNVIDIALogo';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -41,8 +27,46 @@ function getIcon(type: string) {
     case 'rag':
       return <FolderSearch2Icon color="skyblue" />;
     default:
-      return null;
+      tint = 'var(--joy-palette-background-surface)';
   }
+
+  // Now mix the Tint color with the background color
+  // so that this works in dark and light mode
+  return (
+    'color-mix(in srgb, ' +
+    tint +
+    ', var(--joy-palette-background-surface) 50%)'
+  );
+}
+
+function mapArchitectureToIcon(arch) {
+  switch (arch) {
+    case 'cuda':
+      return (
+        <>
+          <TinyNVIDIALogo /> CUDA
+        </>
+      );
+    case 'mlx':
+      return <TinyMLXLogo />;
+    default:
+      return (
+        <Chip key={arch} color="primary">
+          {arch}
+        </Chip>
+      );
+  }
+}
+
+function ShowArchitectures({ architectures }) {
+  if (!architectures) return null;
+  return (
+    <>
+      {architectures.map((arch) => (
+        <div key={arch}>{mapArchitectureToIcon(arch)}</div>
+      ))}
+    </>
+  );
 }
 
 export default function PluginCard({
@@ -87,12 +111,27 @@ export default function PluginCard({
 
             <Typography level="body-md">{plugin.description}</Typography>
           </Box>
+          {plugin?.supported_hardware_architectures && (
+            <Box sx={{ mt: 1 }}>
+              <Typography level="title-sm" fontSize="sm">
+                Supported Architectures:
+              </Typography>
+              <Stack flexDirection={'row'} gap={1}>
+                <ShowArchitectures
+                  architectures={plugin?.supported_hardware_architectures}
+                />
+              </Stack>
+            </Box>
+          )}
+
           <Box
             sx={{
               display: 'flex',
               flexDirection: 'row',
               mt: 1,
               justifyContent: 'flex-end',
+              alignItems: 'center',
+              gap: 1,
             }}
           >
             {!download && (
@@ -130,8 +169,8 @@ export default function PluginCard({
                       await fetch(
                         chatAPI.Endpoints.Experiment.DeletePlugin(
                           experimentInfo?.id,
-                          plugin?.uniqueId
-                        )
+                          plugin?.uniqueId,
+                        ),
                       );
                       parentMutate();
                     }
@@ -151,8 +190,8 @@ export default function PluginCard({
                 await fetch(
                   chatAPI.Endpoints.Experiment.InstallPlugin(
                     experimentInfo?.id,
-                    plugin.uniqueId
-                  )
+                    plugin.uniqueId,
+                  ),
                 );
                 setInstalling(null);
                 parentMutate();
