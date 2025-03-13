73d24c61befcc93c713898ef84e3b749a7fe2c6f
Tony Salomone <dadmobile@gmail.com>
2025-03-12 20:00:35 -0400

    Cloudapp: Hack to add a login window so server can be set for web until loginmodal fixed.

--- src/renderer/components/Connect/LoginModal.tsx
+++ src/renderer/components/Connect/LoginModal.tsx
@@ -42,6 +42,7 @@ export default function LoginModal({
   const [recentSSHConnections, setRecentSSHConnections] = React.useState<
     string[]
   >([]);
+  const [cloudModalOpen, setCloudModalOpen] = React.useState<boolean>(true);
 
   const [host, setHost] = useState('');
 
@@ -95,6 +96,89 @@ export default function LoginModal({
     } else {
       setFailed(true);
     }
+  }
+
+  // SUPER TEMP HACK: Skip login window if this isn't the electron app
+  // instead hack up a new login screen
+  if (!window.storage) {
+    return (
+      <Modal open={cloudModalOpen}>
+        <ModalDialog
+          aria-labelledby="basic-modal-dialog-title"
+          aria-describedby="basic-modal-dialog-description"
+          sx={{
+            top: '5vh', // Sit 20% from the top of the screen
+            margin: 'auto',
+            transform: 'translateX(-50%)', // This undoes the default translateY that centers vertically
+            width: '80vw',
+            // maxWidth: '700px',
+            height: '90vh',
+          }}
+        >
+          <form
+            onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+              event.preventDefault();
+
+              const server = event.currentTarget.elements[0].value
+                .trim()
+                .replace(/\/+$/, '');
+              const port = event.currentTarget.elements[1].value.replace(
+                /[\s]+/g,
+                ''
+              );
+              if (!window.TransformerLab) {
+                console.log("Crap TransfomerLab isn't set yet.");
+                return;
+              }
+              // eslint-disable-next-line prefer-template
+              const fullServer = 'http://' + server + ':' + port + '/';
+
+              window.TransformerLab = {};
+              window.TransformerLab.API_URL = fullServer;
+
+              setCloudModalOpen(false);
+            }}
+          >
+            <Stack spacing={2}>
+              <FormControl>
+                <FormLabel>Server URL</FormLabel>
+                <Input autoFocus required placeholder="192.168.1.100" />
+                <FormHelperText>
+                  Do not include http:// in the URL.
+                </FormHelperText>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Server Port</FormLabel>
+                <Input required defaultValue="8338" placeholder="8338" />
+              </FormControl>
+              <Button
+                type="submit"
+                startDecorator={
+                  checking && (
+                    <CircularProgress
+                      variant="solid"
+                      thickness={2}
+                      sx={{
+                        '--CircularProgress-size': '16px',
+                        color: 'white',
+                      }}
+                    />
+                  )
+                }
+                sx={{ p: 1 }}
+              >
+                Submit
+              </Button>
+              {failed && (
+                <div style={{ color: 'var(--joy-palette-danger-600)' }}>
+                  Couldn&apos;t connect to server. Please try a different URL.
+                </div>
+              )}
+            </Stack>
+          </form>
+        </ModalDialog>
+      </Modal>
+    );
   }
 
   return (
