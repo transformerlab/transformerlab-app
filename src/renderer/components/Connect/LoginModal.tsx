b044fa187cd011e92104f0231615172580a74f31
Tony Salomone <dadmobile@gmail.com>
2025-03-18 19:27:42 -0400

    Webapp: Remove unneeded state variable from LoginModal now that layout is simpler

--- src/renderer/components/Connect/LoginModal.tsx
+++ src/renderer/components/Connect/LoginModal.tsx
@@ -42,7 +42,6 @@ export default function LoginModal({
   const [recentSSHConnections, setRecentSSHConnections] = React.useState<
     string[]
   >([]);
-  const [cloudModalOpen, setCloudModalOpen] = React.useState<boolean>(true);
 
   const [host, setHost] = useState('');
 
