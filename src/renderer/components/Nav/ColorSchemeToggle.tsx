d6afbd6c1172962e0480a1a188442eec41ea9298
ali asaria <aliasaria@users.noreply.github.com>
2025-03-21 13:55:29 -0400

    Remove ipc for darkmode (it wasn't used)

--- src/renderer/components/Nav/ColorSchemeToggle.tsx
+++ src/renderer/components/Nav/ColorSchemeToggle.tsx
@@ -8,17 +8,10 @@ export default function ColorSchemeToggle({ themeSetter }) {
   const { mode, setMode } = useColorScheme();
   const [count, setCount] = useState(0);
 
-  // window.darkMode.onUpdate((isDarkMode) => {
-  //   console.log('Dark mode is now', isDarkMode ? 'on' : 'off');
-  //   // setMode(isDarkMode ? 'dark' : 'light');
-  // });
-
   return (
     <IconButton
       variant="plain"
       onClick={async () => {
-        // const isDarkMode = await window.darkMode.toggle();
-        // console.log('Dark mode is now', isDarkMode ? 'on' : 'off');
         setMode(mode == 'light' ? 'dark' : 'light');
         setCount(count + 1);
         if (count > 10 && count % 2 == 0) {
