91ee12329dcb81855bc70675c1c3532b555fbc73
ali asaria <aliasaria@users.noreply.github.com>
2025-03-27 11:52:42 -0400

    Initial recipes landing scree

--- src/renderer/components/Recipes/index.tsx
+++ src/renderer/components/Recipes/index.tsx
@@ -2,11 +2,45 @@
 import Modal from '@mui/joy/Modal';
 import ModalDialog from '@mui/joy/ModalDialog';
 import { Button, Grid, ModalClose, Typography } from '@mui/joy';
+import { Button, Grid, ModalClose, Typography } from '@mui/joy';
 import * as chatAPI from 'renderer/lib/transformerlab-api-sdk';
 import RecipeCard from './RecipeCard';
 import NewExperimentModal from './NewExperimentModal';
 import { useState } from 'react';
-import recipeDetails from './recipeDetails'; // Import the JSON
+
+// Create a fake recipe details array with Title, Description:
+const recipeDetails = [
+  {
+    id: 1,
+    title: 'Train a Model From Scratch',
+    description:
+      'Build a new machine learning model from the ground up using Nanotron. Ideal for custom use cases and datasets.',
+  },
+  {
+    id: 2,
+    title: 'Fine-tune an Existing Model',
+    description:
+      'Adapt a pre-trained model to your specific needs using LoRA. Save time and resources by leveraging existing knowledge.',
+  },
+  {
+    id: 3,
+    title: 'Evaluate a Model',
+    description:
+      'Assess the performance of your model using Eleuther Labs AI Evaluation Harness. Gain insights into accuracy and reliability.',
+  },
+  {
+    id: 4,
+    title: 'Convert a Model to the MLX Format',
+    description:
+      'Transform your model into the MLX format for compatibility with various deployment environments.',
+  },
+  {
+    id: 5,
+    title: 'Quantize a Model',
+    description:
+      'Optimize your model for faster inference and reduced size using Nanotron’s quantization tools.',
+  },
+];
 
 export default function RecipesModal({
   modalOpen,
@@ -28,30 +62,26 @@ export default function RecipesModal({
             top: '5vh', // Sit 20% from the top of the screen
             margin: 'auto',
             transform: 'translateX(-50%)', // This undoes the default translateY that centers vertically
+            width: '80vw',
+            // maxWidth: '700px',
             height: '90vh',
-            width: '100vw',
           }}
         >
           <ModalClose onClick={() => setModalOpen(false)} />
-          <Typography level="h2" mt={2}>
-            What do you want to do?
-          </Typography>
-          <Typography level="body-lg" mb={4}>
-            Start a new experiment using one of the recipes below, or start from
-            scratch.
+          <Typography level="h2" mb={2}>
+            👋 Welcome to Transformer Lab! What do you want to do?
           </Typography>
           <Grid
             container
-            spacing={3}
+            spacing={2}
             sx={{
               flexGrow: 1,
-              justifyContent: 'flex-start',
+              justifyContent: 'center',
               alignContent: 'flex-start',
               overflow: 'auto',
-              width: '100%',
             }}
           >
-            <Grid key={0} xs={12} sm={6} md={4} lg={3}>
+            <Grid key={0}>
               <RecipeCard
                 recipeDetails={{
                   title: 'Create a Blank Experiment',
@@ -64,7 +94,7 @@ export default function RecipesModal({
               />
             </Grid>
             {recipeDetails.map((recipe) => (
-              <Grid key={recipe.id} xs={12} sm={6} md={4} lg={3}>
+              <Grid key={recipe.id}>
                 <RecipeCard recipeDetails={recipe} />
               </Grid>
             ))}
