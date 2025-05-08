90db07cffc1b30b61e887f353c4c5ab4afbf12e2
ali asaria <aliasaria@users.noreply.github.com>
2025-05-07 20:56:54 -0400

    move last functions to functions.ts

--- src/renderer/lib/api-client/functions.ts
+++ src/renderer/lib/api-client/functions.ts
@@ -174,3 +174,136 @@ export async function killWorker() {
     return undefined;
   }
 }
+
+export function GET_EXPERIMENT_UPDATE_CONFIG_URL(
+  id: string,
+  key: string,
+  value: string | undefined,
+) {
+  if (value === undefined) {
+    value = '';
+  }
+  return (
+    API_URL() +
+    'experiment/' +
+    id +
+    '/update_config' +
+    '?key=' +
+    key +
+    '&value=' +
+    value
+  );
+}
+
+export async function EXPERIMENT_ADD_EVALUATION(
+  id: string,
+  name: string,
+  pluginId: string,
+  scriptParameters: any,
+) {
+  const newPlugin = {
+    name: name,
+    plugin: pluginId,
+    script_parameters: scriptParameters,
+  };
+
+  const response = await fetch(API_URL() + 'experiment/' + id + '/evals/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      accept: 'application/json',
+    },
+    body: JSON.stringify(newPlugin),
+  });
+  const result = await response.json();
+  return result;
+}
+
+export async function EXPERIMENT_EDIT_EVALUATION(
+  id: string,
+  evalName: string,
+  scriptParameters: any,
+) {
+  const newPlugin = {
+    evalName: evalName,
+    script_parameters: scriptParameters,
+  };
+
+  const response = await fetch(API_URL() + 'experiment/' + id + '/evals/edit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      accept: 'application/json',
+    },
+    body: JSON.stringify(newPlugin),
+  });
+  const result = await response.json();
+  return result;
+}
+
+export async function EXPERIMENT_ADD_GENERATION(
+  id: string,
+  name: string,
+  pluginId: string,
+  scriptParameters: any,
+) {
+  const newPlugin = {
+    name: name,
+    plugin: pluginId,
+    script_parameters: scriptParameters,
+  };
+
+  const response = await fetch(
+    API_URL() + 'experiment/' + id + '/generations/add',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+      body: JSON.stringify(newPlugin),
+    },
+  );
+  const result = await response.json();
+  return result;
+}
+
+export async function EXPERIMENT_EDIT_GENERATION(
+  id: string,
+  evalName: string,
+  scriptParameters: any,
+) {
+  const newPlugin = {
+    evalName: evalName,
+    script_parameters: scriptParameters,
+  };
+
+  const response = await fetch(
+    API_URL() + 'experiment/' + id + '/generations/edit',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+      body: JSON.stringify(newPlugin),
+    },
+  );
+  const result = await response.json();
+  return result;
+}
+
+export function TEMPLATE_FOR_MODEL_URL(model: string) {
+  return `${API_URL()}model/get_conversation_template?model=${model}`;
+}
+
+export async function getTemplateForModel(modelName: string) {
+  if (!modelName) {
+    return null;
+  }
+  const model = modelName.split('/')[1];
+  const response = await fetch(TEMPLATE_FOR_MODEL_URL(model));
+  const result = await response.json();
+
+  return result;
+}
