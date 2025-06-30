a0429b5ae2225283f6d6044885e5ddec3a7aef7c
Tony Salomone <dadmobile@gmail.com>
2025-06-30 16:59:04 -0400

    Fix crash when saving experiment notes

--- src/renderer/components/Experiment/ExperimentNotes.tsx
+++ src/renderer/components/Experiment/ExperimentNotes.tsx
@@ -70,7 +70,7 @@ export default function ExperimentNotes({ experimentInfo }) {
       },
     )
       .then(() => {
-        mutate(value);
+        mutate();
         setIsEditing(false);
         return true;
       })
