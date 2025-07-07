e737042fcbc37611e36a7b7f2432c5f601cdf46d
ali asaria <aliasaria@users.noreply.github.com>
2025-07-07 10:29:36 -0400

    add more logging

--- src/renderer/components/Shared/analytics/AnalyticsContext.js
+++ src/renderer/components/Shared/analytics/AnalyticsContext.js
@@ -8,13 +8,18 @@ import { appContextPlugin } from './appContextPlugin'; // Import the app context
 export const analytics = new AnalyticsBrowser();
 
 async function maybeLoadAnalytics() {
+  console.log('[Analytics] Initializing Analytics...');
   if (window.platform?.environment === 'development') {
-    console.log('Analytics tracking is disabled in development mode.');
+    console.log(
+      '[Analytics] Analytics tracking is disabled in development mode.',
+    );
     return;
   }
   const doNotTrack = await window.storage.get('DO_NOT_TRACK');
   if (doNotTrack) {
-    console.log('User has opted out. All Segment tracking is disabled.');
+    console.log(
+      '[Analytics] User has opted out. All Segment tracking is disabled.',
+    );
     return;
   }
   analytics.load({ writeKey: 'UYXFr71CWmsdxDqki5oFXIs2PSR5XGCE' }); // destinations loaded, enqueued events are flushed
