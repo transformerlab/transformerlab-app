ae7779536da48e5acc61033f473068b400b7d410
Mina Parham <mina.parham766@gmail.com>
2025-10-23 16:54:09 -0400

    Prettier

--- src/renderer/components/Experiment/Train/ViewCheckpointsModal.tsx
+++ src/renderer/components/Experiment/Train/ViewCheckpointsModal.tsx
@@ -35,7 +35,7 @@ export default function ViewCheckpointsModal({ open, onClose, jobId }) {
           body: JSON.stringify({
             checkpoint_name: checkpoint.filename,
           }),
-        }
+        },
       );
 
       if (response.ok) {
