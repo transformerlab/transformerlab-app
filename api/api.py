8e36e969b7dc40913b4ea72a2c0a14f062e62584
deep1401 <gandhi0869@gmail.com>
2025-12-01 14:12:52 -0700

    Change app setup to have allow credentials as False and change definitions of calls on frontend

--- api/api.py
+++ api/api.py
@@ -188,9 +188,8 @@ app = fastapi.FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    # Restrict origins so credentialed requests (cookies) are allowed
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,  # Not needed - we use Bearer token auth, not cookies
     allow_methods=["*"],
     allow_headers=["*"],
 )
