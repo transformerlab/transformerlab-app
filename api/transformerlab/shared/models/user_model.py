43c239ecfcdf170102bde8729f6663e041d9071a
deep1401 <gandhi0869@gmail.com>
2025-12-01 09:41:16 -0700

    add comment explaining why unique is necessary

--- api/transformerlab/shared/models/user_model.py
+++ api/transformerlab/shared/models/user_model.py
@@ -49,6 +49,7 @@ class SQLAlchemyUserDatabaseWithOAuth(SQLAlchemyUserDatabase):
             .where(OAuthAccount.oauth_name == oauth, OAuthAccount.account_id == account_id)
         )
         result = await self.session.execute(statement)
+        # The unique() is used to ensure that we only get one user back. The `lazy=joined` in the table definition makes sure it returns a collection and we need to pick a single user.
         user = result.unique().scalar_one_or_none()
         return user
 
