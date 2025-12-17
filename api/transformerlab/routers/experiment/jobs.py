19e03c563a2450210fd5d4ecf489bbc1c806b4b3
Mina Parham <mina.parham766@gmail.com>
2025-12-17 14:31:17 -0500

    Fix provider log issue

--- api/transformerlab/routers/experiment/jobs.py
+++ api/transformerlab/routers/experiment/jobs.py
@@ -279,6 +279,11 @@ async def get_provider_job_logs(
         provider_job_ids = job_data.get("provider_job_ids")
         if isinstance(provider_job_ids, list) and provider_job_ids:
             provider_job_id = provider_job_ids[-1]
+    
+    if provider_job_id is None:
+        provider_launch_result = job_data.get("provider_launch_result")
+        if isinstance(provider_launch_result, dict):
+            provider_job_id = provider_launch_result.get("job_id")
 
     if provider_job_id is None:
         try:
