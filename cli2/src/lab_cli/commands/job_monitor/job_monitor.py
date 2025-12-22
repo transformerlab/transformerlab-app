4aa8a6726fa5a0c2ca0df26d014e621525b415e9
ali asaria <aliasaria@users.noreply.github.com>
2025-12-22 12:21:27 -0500

    ruff

--- cli2/src/lab_cli/commands/job_monitor/job_monitor.py
+++ cli2/src/lab_cli/commands/job_monitor/job_monitor.py
@@ -1,19 +1,3 @@
-from textual.app import ComposeResult
-from textual.widgets import (
-    Static,
-    Label,
-    LoadingIndicator,
-    Button,
-    Select,
-)
-from textual.containers import Vertical
-from textual.screen import ModalScreen
-from textual import work
-from textual.reactive import reactive
-
-from lab_cli.util import api
-from lab_cli.util.config import get_current_experiment, set_config
-
 from lab_cli.commands.job_monitor.JobMonitorApp import JobMonitorApp
 
 
