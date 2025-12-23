8efc76fe8f403f62130a7a9da39202023c292c05
deep1401 <gandhi0869@gmail.com>
2025-12-23 10:45:00 -0700

    quotas api stuff

--- api/api.py
+++ api/api.py
@@ -63,6 +63,7 @@ from transformerlab.routers import (  # noqa: E402
     compute_provider,
     auth,
     api_keys,
+    quota,
 )
 from transformerlab.routers.auth import get_user_and_team  # noqa: E402
 import torch  # noqa: E402
@@ -255,6 +256,7 @@ app.include_router(teams.router, dependencies=[Depends(get_user_and_team)])
 app.include_router(compute_provider.router)
 app.include_router(auth.router)
 app.include_router(api_keys.router)
+app.include_router(quota.router)
 
 controller_process = None
 worker_process = None
