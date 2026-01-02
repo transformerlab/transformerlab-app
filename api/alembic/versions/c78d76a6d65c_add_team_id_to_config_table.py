64df92d1f03869fb6fe11b5213db87f8096e03ed
deep1401 <gandhi0869@gmail.com>
2026-01-02 14:32:20 -0700

    fix down revision

--- api/alembic/versions/c78d76a6d65c_add_team_id_to_config_table.py
+++ api/alembic/versions/c78d76a6d65c_add_team_id_to_config_table.py
@@ -1,7 +1,7 @@
 """add_team_id_to_config_table
 
 Revision ID: c78d76a6d65c
-Revises: f278bbaa6f67
+Revises: 1f7cb465d15a
 Create Date: 2025-12-04 11:23:22.165544
 
 """
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "c78d76a6d65c"
-down_revision: Union[str, Sequence[str], None] = "f278bbaa6f67"
+down_revision: Union[str, Sequence[str], None] = "1f7cb465d15a"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
