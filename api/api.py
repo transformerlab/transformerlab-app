e93b251fc64e8ded5fb67247f485abedaeec8180
Tony Salomone <dadmobile@gmail.com>
2026-01-12 16:11:28 -0500

    Add a default sentry_sdk init if DSN is set in environment

--- api/api.py
+++ api/api.py
@@ -197,6 +197,27 @@ tags_metadata = [
     },
 ]
 
+if os.getenv("SENTRY_DSN"):
+    # Import only if SENTRY_DSN is set.
+    # This way we can avoid making sentry_sdk a mandatory dependency.
+    import sentry_sdk
+
+    sentry_sdk.init(
+        dsn=os.environ["SENTRY_DSN"],
+        # integrations=[FastApiIntegration()],
+        # Enable sending logs to Sentry
+        enable_logs=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
+        traces_sample_rate=1.0,
+        # Set profile_session_sample_rate to 1.0 to profile 100%
+        # of profile sessions.
+        profile_session_sample_rate=1.0,
+        # Set profile_lifecycle to "trace" to automatically
+        # run the profiler on when there is an active transaction
+        profile_lifecycle="trace",
+    )
+
 app = fastapi.FastAPI(
     title="Transformerlab API",
     description=description,
