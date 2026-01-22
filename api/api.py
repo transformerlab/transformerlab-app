72ee69e294d482cedc53a9520b3bd3c6bdc43323
Tony Salomone <dadmobile@gmail.com>
2026-01-22 15:35:09 -0500

    Add back important sentry parameters

--- api/api.py
+++ api/api.py
@@ -196,7 +196,17 @@ if os.getenv("SENTRY_DSN"):
     sentry_sdk.init(
         dsn=os.environ["SENTRY_DSN"],
         integrations=[FastApiIntegration()],
-    )
+        # Enable sending logs to Sentry
+        enable_logs=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
+        traces_sample_rate=1.0,
+        # Set profile_session_sample_rate to 1.0 to profile 100%
+        # of profile sessions.
+        profile_session_sample_rate=1.0,
+        # Set profile_lifecycle to "trace" to automatically
+        # run the profiler on when there is an active transaction
+        profile_lifecycle="trace"
 
 app = fastapi.FastAPI(
     title="Transformerlab API",
