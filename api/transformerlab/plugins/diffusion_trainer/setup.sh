99e483e8ff2b76bf145612ad912a248925aaae2b
paramthakkar123 <paramthakkar864@gmail.com>
2026-02-05 23:45:15 +0530

    Updates

--- api/transformerlab/plugins/diffusion_trainer/setup.sh
+++ api/transformerlab/plugins/diffusion_trainer/setup.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Install compatible torch and torchvision first to avoid version conflicts
+uv pip install torch torchvision
+
+# Install PEFT and diffsynth
 uv pip install "peft>=0.15.0" diffsynth
 
 # Only install xformers for non-rocm instances
