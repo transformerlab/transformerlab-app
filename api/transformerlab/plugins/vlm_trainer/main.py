5a830a5b577da66d8b461800957c4996ba264f3e
deep1401 <gandhi0869@gmail.com>
2026-02-11 11:02:11 -0700

    Avoid Peft Model config from being passed twice in new TRL paradigm

--- api/transformerlab/plugins/vlm_trainer/main.py
+++ api/transformerlab/plugins/vlm_trainer/main.py
@@ -160,7 +160,6 @@ def train_vlm():
         args=args,
         train_dataset=dataset,
         data_collator=collate_fn,
-        peft_config=peft_config,
         processing_class=processor.tokenizer,
         callbacks=[progress_callback],
     )
