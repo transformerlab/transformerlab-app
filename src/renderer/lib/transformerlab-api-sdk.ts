11947dd492250f12289142bb4ae40afd50e0ac14
deep1401 <gandhi0869@gmail.com>
2026-03-19 12:58:12 -0600

    fix

--- src/renderer/lib/transformerlab-api-sdk.ts
+++ src/renderer/lib/transformerlab-api-sdk.ts
@@ -6,23 +6,6 @@
 export * from './api-client/endpoints';
 
 export {
-<<<<<<< fix/remove-interactcomp-cache-compprov
-=======
-  stopStreamingResponse,
-  sendAndReceiveStreaming,
-  sendCompletion,
-  sendCompletionReactWay,
-  sendBatchedCompletion,
-  sendBatchedChat,
-  callTool,
-  getToolsForCompletions,
-  tokenize,
-  countTokens,
-  countChatTokens,
-} from './api-client/chat';
-
-export {
->>>>>>> main
   GET_EXPERIMENT_UPDATE_CONFIG_URL,
   EXPERIMENT_ADD_EVALUATION,
   TEMPLATE_FOR_MODEL_URL,
