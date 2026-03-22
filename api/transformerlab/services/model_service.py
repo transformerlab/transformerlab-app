5a3ed68088e4b36adaf8bafbae750ecfd48188f0
Tony Salomone <dadmobile@gmail.com>
2026-03-22 15:00:49 -0400

    Remove all model provenance functions

--- api/transformerlab/services/model_service.py
+++ api/transformerlab/services/model_service.py
@@ -6,16 +6,11 @@ Service layer for working with models.
 
 from transformerlab.models import localmodel
 
+from lab import storage, get_models_dir
+
 
 async def list_installed_models(embedding: bool = False) -> list:
     """
     Legacy function for getting a list of models from all sources.
     """
     return await localmodel.LocalModelStore().list_models(embedding)
-
-
-async def list_model_provenance(model_id: str) -> list:
-    """
-    Return a list of the provenance of a model with the unique ID model_id.
-    """
-    return await localmodel.LocalModelStore().list_model_provenance(model_id)
