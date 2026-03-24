c6ffc118427c848a03416908e3122840c5e17ca1
ali asaria <aliasaria@users.noreply.github.com>
2026-03-24 13:26:59 -0400

    Fix prettier formatting in QueueTaskModal

--- src/renderer/components/Experiment/Tasks/QueueTaskModal.tsx
+++ src/renderer/components/Experiment/Tasks/QueueTaskModal.tsx
@@ -1334,7 +1334,6 @@ export default function QueueTaskModal({
                   </FormHelperText>
                 </FormControl>
               )}
-
             </Stack>
 
             <Divider />
