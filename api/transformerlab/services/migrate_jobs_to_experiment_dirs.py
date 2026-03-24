1bd62df0c344338ab8bc2e1e84daa0b75dc80afd
Tony Salomone <dadmobile@gmail.com>
2026-03-24 17:04:05 -0400

    Ruff

--- api/transformerlab/services/migrate_jobs_to_experiment_dirs.py
+++ api/transformerlab/services/migrate_jobs_to_experiment_dirs.py
@@ -17,7 +17,6 @@ from __future__ import annotations
 import asyncio
 import json
 import logging
-import os
 from typing import Any, Optional
 
 from lab.dirs import get_workspace_dir, set_organization_id as lab_set_org_id
